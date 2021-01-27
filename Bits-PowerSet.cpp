cb298ad2a82d6846cea739f10202db051fa7fd40
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-01-27 21:07:13 +0530

    Add files via upload

---
+++ Bits-PowerSet.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+    vector<vector<int>> subsets(vector<int>& num) {
+       int size=num.size();
+    int subsetNum=(1<<size);
+    vector<vector<int> >allSubsets;
+    for(int subsetMask=0;subsetMask<subsetNum;++subsetMask){
+        vector<int>subset;
+        for(int i=0;i<size;++i){
+            if((subsetMask & (1<<i))!=0)
+                
+            {
+                subset.push_back(num[i]);
+            }
+             
+        }
+        allSubsets.push_back(subset);
+    }
+
+    return allSubsets;   
+    }
+
+
+int main(){
+    vector<int> v{1,2,3};
+
+    vector<vector<int>>ans=subsets(v);
+    for(auto &v:ans){
+      
+        for(int &u:v){
+            cout<<"["<<u<<"]";
+        }
+        cout<<" ";
+    }
+    
+}
