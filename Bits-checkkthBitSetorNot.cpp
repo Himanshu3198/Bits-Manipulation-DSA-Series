6668e0f5f8516e109108cef2f81d7a79e86687e2
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-04-19 13:10:43 +0530

    Add files via upload

---
+++ Bits-checkkthBitSetorNot.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+  
+void isKthBitSet(int n, int k)
+{
+    if (n & (1 << (k-1)))
+        cout << "SET";
+    else
+        cout << "NOT SET";
+}
+  
+// Driver code
+int main()
+{
+    int n = 5, k = 1;
+    isKthBitSet(n, k);
+    return 0;
+}
